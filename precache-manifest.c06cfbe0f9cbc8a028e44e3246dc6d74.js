ef0e15847d5099742b691f46e58c05f80e7dacbe
Renan Roberto <renanrobe@gmail.com>
2020-07-21 17:12:04 -0300

    Updates

---
+++ precache-manifest.c06cfbe0f9cbc8a028e44e3246dc6d74.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a2733d3bd641c1ec035b196dcc68f04",
+    "url": "/luizalabsfront/index.html"
+  },
+  {
+    "revision": "cd4a81ac8046f7650f0c",
+    "url": "/luizalabsfront/static/css/main.b9f5b5fd.chunk.css"
+  },
+  {
+    "revision": "c7bc96556cfecec340a7",
+    "url": "/luizalabsfront/static/js/2.288d916f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/luizalabsfront/static/js/2.288d916f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd4a81ac8046f7650f0c",
+    "url": "/luizalabsfront/static/js/main.b24d1fcf.chunk.js"
+  },
+  {
+    "revision": "ced1335ee67707bedfb7",
+    "url": "/luizalabsfront/static/js/runtime-main.f68f5047.js"
+  },
+  {
+    "revision": "0ad1e3aee93076f9e922ce6898a1f4bd",
+    "url": "/luizalabsfront/static/media/ico-heart-false.0ad1e3ae.svg"
+  },
+  {
+    "revision": "148062e99ff081e5b85f1cb0af01ad5f",
+    "url": "/luizalabsfront/static/media/ico-heart-true.148062e9.svg"
+  },
+  {
+    "revision": "be88535eb74143185015f8a9343855fd",
+    "url": "/luizalabsfront/static/media/ico-search.be88535e.svg"
+  },
+  {
+    "revision": "f78c6ac2fd2796c1514d5ad4323e4c45",
+    "url": "/luizalabsfront/static/media/ico-superhero.f78c6ac2.svg"
+  },
+  {
+    "revision": "b3447ab5f8d79d709108167b3748be21",
+    "url": "/luizalabsfront/static/media/ico-toggle-off.b3447ab5.svg"
+  },
+  {
+    "revision": "aab16b441452282e8606f6d64c2671d3",
+    "url": "/luizalabsfront/static/media/ico-toggle-on.aab16b44.svg"
+  },
+  {
+    "revision": "fec1f18d77627f2b929d7123c6c379de",
+    "url": "/luizalabsfront/static/media/logo.fec1f18d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/luizalabsfront/precache-manifest.a14cd2b669efd25de13d20c3dfb70db5.js"
+  "/luizalabsfront/precache-manifest.c06cfbe0f9cbc8a028e44e3246dc6d74.js"
 );
 
 self.addEventListener('message', (event) => {
